6b3fd0a50158fcdfe6ce0a9cb68a2147138149b3
Gregory Orange <8380355+gregoryo2014@users.noreply.github.com>
2019-09-10 13:56:26 +0800

    Process a CSV and output multiple HTML stanzas

--- generate-html.sh
+++ generate-html.sh
@@ -1,14 +1,33 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 #
-# Generate HTML for Weebly talks page
+# Author: Gregory Orange
 #
-# First edit: Just generate new text to add to existing document(s)
+# Generate HTML for Weebly talks page, using data file talks-data.csv, which
+# should be in the format below. Note the file should exist in the compressed
+# audio files directory, and its name should start with the date stamp. Lines
+# should be in reverse chronological order, and should be preserved - just add
+# new ones.
+#
+# 20190910-filename2.mp3,Name Of Speaker 2,Topic of talk 2
+# 20190909.filename.mp3,Name Of Speaker,Topic of talk
+#
+####################
+# HOW TO USE:
+####################
+# 1. Populate talks-data.csv
+# 2. ./generate-html.sh > data/10-data
+# 3. Concatenate the data to the clipboard:
+#   * Ubuntu: cat data/* | xclip -sel c
+#   * Mac:    cat data/* | pbcopy
+# 4. Paste into the talks page HTML object.
+# 5. Click Publish.
+#
+# Separately (before or after), upload the audio files to the "Talks holding page" on
+# Weebly.
+####################
 #
 set -o nounset
 set -o errexit
-
-IFS='
-'
 
 usage() {
   cat <<- EOF
@@ -17,32 +36,29 @@ usage() {
 EOF
 }
 
-while getopts "f:hs:t:" option; do
-  case $option in
-    f) UPLOAD_FILE=$OPTARG;;
-    s) SPEAKER=$OPTARG;;
-    t) TOPIC=$OPTARG;;
-    h) usage; exit 0;;
-  esac
-done
-
-FILENAME=$(basename $UPLOAD_FILE)
-DATE=$(echo $FILENAME | cut -c 1-8)
-SIZE=$(du -m "$UPLOAD_FILE" | cut -f 1)
-
-[ -f "$UPLOAD_FILE" ] || {
-  echo "Couldn't find file: $UPLOAD_FILE"
-  exit 1
-}
-
-cat <<EOF
 ############################################################
+IFS='
+'
+SCRIPT_DIR=$(dirname $0)
+DATA_FILE="$SCRIPT_DIR/talks-data.csv"
+TALKS_DIR="$SCRIPT_DIR/../compressed audio files"
+
+# process file
+for line in $(cat $DATA_FILE); do
+  FILENAME=$(echo $line | cut -f1 -d,)
+  SPEAKER=$(echo $line | cut -f2 -d,)
+  TOPIC=$(echo $line | cut -f3 -d,)
+  UPLOAD_FILE="$TALKS_DIR/$FILENAME"
+  FILENAME=$(basename $UPLOAD_FILE)
+  DATE=$(echo $FILENAME | cut -c 1-8)
+  SIZE=$(du -m "$UPLOAD_FILE" | cut -f 1)
+
+  cat <<EOF
 <TR CLASS=R> <TD CLASS=C1>$DATE</TD> <TD CLASS=C2>$SPEAKER</TD>
 <TD CLASS=C3>$TOPIC</TD> <TD CLASS=C4>
-<A TARGET="MHCC" HREF="/uploads/6/0/7/6/6076729/20190818_cm.mp3">$FILENAME</A>
+<A TARGET="MHCC" HREF="/uploads/6/0/7/6/6076729/$FILENAME">$FILENAME</A>
 </TD><TD CLASS=C5>${SIZE}MB</TD></TR>
 
-############################################################
 EOF
 
-unset IFS
+done
