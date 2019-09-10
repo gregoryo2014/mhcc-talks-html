d32044fecd9286cc3e6fbfdc331b04802174b591
Gregory Orange <8380355+gregoryo2014@users.noreply.github.com>
2019-09-10 20:11:11 +0800

    Moved doco to README

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
 # mhcc-talks-html
-Generate HTML for http://www.mhcc.asn.au/talks.html
+
+Generate HTML for http://www.mhcc.asn.au/talks.html using data file
+talks-data.csv, which should be in the format below. Note the file should
+exist in the compressed audio files directory, and its name should start with
+the date stamp. Lines should be in reverse chronological order, and should be
+preserved - just add new ones at the top.
+
+Data files are expected to be in the directory '../compressed audio files'.
+
+```# Date,Speaker,Topic,Filename 1[,Filename 2[,...]]
+10/09/2019,Name Of Speaker 2,Topic of talk 2,20190910-filename2.mp3,20190910-filename2.pdf
+09/09/2019,Name Of Speaker,Topic of talk,20190909.filename.mp3```
+
+## HOW TO USE:
+1. Populate talks-data.csv
+1. ./generate-html.sh > data/10-data
+1. Concatenate the data to the clipboard:
+  * Ubuntu: cat data/* | xclip -sel c
+  * Mac:    cat data/* | pbcopy
+1. Paste into the talks page HTML object.
+1. Click Publish.
+
+Separately (before or after), upload the audio files to the "Talks holding page" on
+Weebly.
--- generate-html.py
+++ generate-html.py
@@ -1,31 +1,6 @@
 #!/usr/bin/env python
 #
-# Author: Gregory Orange
-#
-# Generate HTML for Weebly talks page, using data file talks-data.csv, which
-# should be in the format below. Note the file should exist in the compressed
-# audio files directory, and its name should start with the date stamp. Lines
-# should be in reverse chronological order, and should be preserved - just add
-# new ones.
-#
-# # Bare filename,Speaker,Topic
-# Name Of Speaker 2,Topic of talk 2,20190910-filename2.mp3,20190910-filename2.pdf
-# Name Of Speaker,Topic of talk,20190909.filename.mp3,
-#
-####################
-# HOW TO USE:
-####################
-# 1. Populate talks-data.csv
-# 2. ./generate-html.sh > data/10-data
-# 3. Concatenate the data to the clipboard:
-#   * Ubuntu: cat data/* | xclip -sel c
-#   * Mac:    cat data/* | pbcopy
-# 4. Paste into the talks page HTML object.
-# 5. Click Publish.
-#
-# Separately (before or after), upload the audio files to the "Talks holding page" on
-# Weebly.
-####################
+# Generate HTML for Weebly talks page. See README.md for details.
 #
 import re, os
 
@@ -46,6 +21,7 @@ for line in open("talks-data.csv", "r"):
     speaker = fields.pop(0)
     topic = fields.pop(0)
     size = 0
+
     # http://wiki.christophchamp.com/index.php?title=Python
     # https://stackoverflow.com/questions/10112614/how-do-i-create-a-multiline-python-string-with-inline-variables
 
@@ -60,6 +36,8 @@ for line in open("talks-data.csv", "r"):
 <A TARGET="MHCC" HREF="/uploads/6/0/7/6/6076729/%s">%s</A><BR />
 """ % (filename, filename)
       path = "../compressed audio files/" + filename
+
+      # Store the biggest file size for output
       this_size = os.path.getsize(path)
       if this_size > size:
         size = this_size
